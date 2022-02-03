a0711d3145929f48a4d5fe758839cbff79da3bca
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-03 16:28:49 -0500

    Updated CSS stylings
    1. Added background colors to visualize layout
    2. Added initial footer layout and stylings
    3. Added initial stylings for LeftTabComponent

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -5,12 +5,59 @@ import "./styles/FooterComponent.css";
 export default function FooterComponent() {
 	return (
 		<footer className="footer-container">
-			<nav>
+			<div className="grid name">
+				<h1>Abrahym Sharf Eldden</h1>
+			</div>
+			<div className="grid title">
+				<p>Software Developer</p>
+			</div>
+			<div className="grid location">
+				<p>New York, NY</p>
+			</div>
+			<div className="grid email">
+				<a href="mailto: asharfeldden@gmail.com">asharfeldden@gmail.com</a>
+			</div>
+			<div className="grid footer-nav">
+				<nav>
+					<ul>
+						<li>01 | home</li>
+						<li>02 | about</li>
+						<li>03 | projects</li>
+						<li>04 | contact</li>
+					</ul>
+				</nav>
+			</div>
+			<div className="grid socials">
 				<ul>
-					<li></li>
-					<li>About</li>
+					<li>LinkedIn Icon</li>
+					<li>GitHub Icon</li>
 				</ul>
-			</nav>
+			</div>
+			<div className="grid copyright">
+				<p>© Abrahym Sharf Eldden</p>
+			</div>
+			{/* <nav>
+				<ul>
+					<li>
+						<div>
+							<h1>Abrahym Sharf Eldden</h1>
+							<p>Software Developer</p>
+						</div>
+					</li>
+					<li>
+						<div>
+							<p>New York, NY</p>
+							<p>asharfeldden@gmail.com</p>
+						</div>
+					</li>
+					<li>
+						<div>
+							<p>New York, NY</p>
+							<p>asharfeldden@gmail.com</p>
+						</div>
+					</li>
+				</ul>
+			</nav> */}
 		</footer>
 	);
 }
