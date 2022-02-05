16e7afdb6969aabf6f81fbd05352a1ca5d63b6a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-02-05 03:39:10 +0000

    Deploy abrahym-sharfeldden/abrahym-sharfeldden.github.io to github.com/abrahym-sharfeldden/abrahym-sharfeldden.github.io.git:gh-pages

---
+++ precache-manifest.413456e3be887ae6f7ef79cff58e4490.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be67332309d23ebf0b8dffe8ba05a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f6206eeb856d19c411e",
+    "url": "/static/css/main.a9a7be8c.chunk.css"
+  },
+  {
+    "revision": "5172c915bd44d946c0f4",
+    "url": "/static/js/2.462e5aac.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.462e5aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6206eeb856d19c411e",
+    "url": "/static/js/main.c9a82e4d.chunk.js"
+  },
+  {
+    "revision": "8b14ff660c773a63dc95",
+    "url": "/static/js/runtime-main.8d297387.js"
+  },
+  {
+    "revision": "ee91e640b5449fb98d9320c877a9866e",
+    "url": "/static/media/fa-brands-400.ee91e640.woff2"
+  },
+  {
+    "revision": "f34b6a2a94e1a01e4c21fa84dcbf6667",
+    "url": "/static/media/fa-brands-400.f34b6a2a.ttf"
+  }
+]);
