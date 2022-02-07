8c23972cbaff044f48151739b399ddb10b2aa6db
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-07 04:18:27 -0500

    Removed debugging console.log from App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "the-new-css-reset/css/reset.css";
 import "./App.css";
 
 const App = () => {
-	console.log("page loaded");
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
 			<HeroComponent />
